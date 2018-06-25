aa0611b501d28fb6b86d73da36b5078c86ff8c8b
Maggiexuiscute <maggiexuiscute@gmail.com>
2018-06-25 11:58:46 +0800

    Unity3D test added

---
+++ Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look at
+
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+	void OnMouseDown () {
+		LookAtTarget.target = target;
+        Camera.main.fieldOfView = Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+	}
+}
